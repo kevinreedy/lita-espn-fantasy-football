fede2d11813e4c833878835cc3b0a4d9d0b04aa0
Kevin Reedy <kevinreedy@gmail.com>
2017-09-05 22:23:32 -0500

    Rubocop fixes

--- lib/lita-espn-fantasy-football.rb
+++ lib/lita-espn-fantasy-football.rb
@@ -1,12 +1,12 @@
-require "lita"
+require 'lita'
 
 Lita.load_locales Dir[File.expand_path(
-  File.join("..", "..", "locales", "*.yml"), __FILE__
+  File.join('..', '..', 'locales', '*.yml'), __FILE__
 )]
 
-require "lita/handlers/espn_fantasy_football"
+require 'lita/handlers/espn_fantasy_football'
 
 Lita::Handlers::EspnFantasyFootball.template_root File.expand_path(
-  File.join("..", "..", "templates"),
- __FILE__
+  File.join('..', '..', 'templates'),
+  __FILE__
 )
