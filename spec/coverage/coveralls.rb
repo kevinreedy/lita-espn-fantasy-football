788da7fea8581032d6d3c0e0351ba803d4622c99
Kevin Reedy <kevinreedy@gmail.com>
2017-09-05 22:44:24 -0500

    Move coveralls to its own spec

---
+++ spec/coverage/coveralls.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start { add_filter '/spec/' }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,3 @@
-require 'simplecov'
-require 'coveralls'
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
-SimpleCov.start { add_filter '/spec/' }
-
 require 'lita-espn-fantasy-football'
 require 'lita/rspec'
 
