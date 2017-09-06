a03c2f5d863de3bd716c94ca07f5ea3cefd3c2bc
Kevin Reedy <kevinreedy@gmail.com>
2017-09-05 22:23:32 -0500

    Rubocop fixes

--- lib/lita/handlers/espn_fantasy_football.rb
+++ lib/lita/handlers/espn_fantasy_football.rb
@@ -1,22 +1,23 @@
-require "nokogiri"
-require "terminal-table"
-require "open-uri"
+require 'nokogiri'
+require 'terminal-table'
+require 'open-uri'
 
 module Lita
   module Handlers
+    # Lita Handler for scraping ESPN's Fantasy Football site
     class EspnFantasyFootball < Handler
       # configs
       config :league_id, required: true
-      config :season_id, default: "2017"
+      config :season_id, default: '2017'
 
       # routes
       route(/^player\s+(.+)/, :command_player, command: true, help: {
-        "player PLAYER NAME" => "Replies information about this football player"
-      })
+              'player PLAYER NAME' => 'Replies information about this football player'
+            })
 
       route(/^score(board)*\s*(\d*)/, :command_scoreboard, command: true, help: {
-        "score WEEK" => "Replies with the scoreboard for the specified week. If WEEK is empty, the current scoreboard is returned"
-      })
+              'score WEEK' => 'Replies with the scoreboard for the specified week. If WEEK is empty, the current scoreboard is returned'
+            })
 
       route(/^sup/, :command_sup, command: true)
       def command_sup(response)
@@ -26,93 +27,93 @@ module Lita
       # chat controllers
       def command_player(response)
         player = response.matches.first.first
-        Lita.logger.debug("#{ response.user.name } asked about player #{ player }")
+        Lita.logger.debug("#{response.user.name} asked about player #{player}")
 
         results = espn_player_search(player)
 
-        if results["rows"].any?
+        if results['rows'].any?
           response.reply(format_results(results))
         else
-          response.reply("No results found for '#{ player }'")
+          response.reply("No results found for '#{player}'")
         end
       end
 
       def command_scoreboard(response)
         week = response.matches.first[1]
-        Lita.logger.debug("#{ response.user.name } requested scoreboard for week '#{ week }'")
+        Lita.logger.debug("#{response.user.name} requested scoreboard for week '#{week}'")
 
         if week.empty? || (week.to_i >= 1 && week.to_i <= 13)
           matchups = espn_scoreboard_scrape(week)
           response.reply(format_results(matchups))
         else
-          response.reply("Please specify a week from 1 - 13")
+          response.reply('Please specify a week from 1 - 13')
         end
       end
 
       # constants
       ESPN_POSITION_MAP = {
-        "qb" => 0,
-        "rb" => 2,
-        "wr" => 4,
-        "te" => 6,
-        "flex" => 23,
-        "d" => 16,
-        "k" => 17
-      }
+        'qb' => 0,
+        'rb' => 2,
+        'wr' => 4,
+        'te' => 6,
+        'flex' => 23,
+        'd' => 16,
+        'k' => 17
+      }.freeze
 
       ESPN_STATUS_MAP = {
-        "ir" => "injured",
-        "o" => "out",
-        "p" => "probable",
-        "q" => "questionable",
-        "sspd" => "suspended"
-      }
+        'ir' => 'injured',
+        'o' => 'out',
+        'p' => 'probable',
+        'q' => 'questionable',
+        'sspd' => 'suspended'
+      }.freeze
 
       # helper methods
-      def get_troll_response
-        responses = [
-          "Your request was bad and you should feel bad.",
-          "Stop wasting my time with your bullshit.",
-          "Was that even English?"
+      def troll_response
+        [
+          'Your request was bad and you should feel bad.',
+          'Stop wasting my time with your bullshit.',
+          'Was that even English?'
         ].sample
       end
 
-      def espn_player_search(query, position="")
+      def espn_player_search(query, position = '')
         resp = {
-          "headers" => [
-            "player",
-            "team",
-            "position",
-            "owner",
-            "projection",
-            "note"
+          'headers' => %w[
+            player
+            team
+            position
+            owner
+            projection
+            note
           ],
-          "rows" => [],
+          'rows' => []
         }
 
-        url = "http://games.espn.go.com/ffl/freeagency?leagueId=#{ config.league_id }&seasonId=#{ config.season_id }&avail=-1&search=#{ query }"
+        url = "http://games.espn.go.com/ffl/freeagency?leagueId=#{config.league_id}&seasonId=#{config.season_id}&avail=-1&search=#{query}"
 
         if position && ESPN_POSITION_MAP[position]
-          url += "&position=#{ ESPN_POSITION_MAP[position] }&slotCategoryId=2"
+          url += "&position=#{ESPN_POSITION_MAP[position]}&slotCategoryId=2"
         end
 
-        Lita.logger.debug("Searching for player at #{ url }")
+        Lita.logger.debug("Searching for player at #{url}")
 
         page = Nokogiri::HTML(open(url))
-        players = page.css("table.playerTableTable.tableBody tr.pncPlayerRow")
+        players = page.css('table.playerTableTable.tableBody tr.pncPlayerRow')
 
         players.each do |p|
-          bio_cell = p.css("td.playertablePlayerName")
+          bio_cell = p.css('td.playertablePlayerName')
 
           # skip blank cells
           next unless bio_cell
 
           # extract info
-          name, bio = bio_cell.text.split(", ")
+          name, bio = bio_cell.text.split(', ')
           chunks = bio.split(/[[:space:]]+/)
 
           if chunks.length < 2
-            Lita.logger.warn("Got a weird cell for player query #{ query }")
+            Lita.logger.warn("Got a weird cell for player query #{query}")
             next
           end
 
@@ -123,22 +124,21 @@ module Lita
             if ESPN_STATUS_MAP.include?(note)
               note = ESPN_STATUS_MAP[note]
             else
-              Lita.logger.warn("Status #{ note } missing from status map")
+              Lita.logger.warn("Status #{note} missing from status map")
             end
           end
 
-          owner = p.css("td")[2].text
-          proj = p.css("td")[13].text
+          owner = p.css('td')[2].text
+          proj = p.css('td')[13].text
 
-          resp["rows"] << {
-            "player" => name,
-            "team" => team,
-            "position" => position,
-            "owner" => owner,
-            "projection" => proj,
-            "note" => note
+          resp['rows'] << {
+            'player' => name,
+            'team' => team,
+            'position' => position,
+            'owner' => owner,
+            'projection' => proj,
+            'note' => note
           }
-
         end
 
         resp
@@ -146,41 +146,39 @@ module Lita
 
       def espn_scoreboard_scrape(week)
         resp = {
-          "headers" => [
-            "team",
-            "score"
+          'headers' => %w[
+            team
+            score
           ],
-          "rows" => []
+          'rows' => []
         }
 
         params = {
-          "leagueId" => config.league_id,
-          "seasonId" => config.season_id
+          'leagueId' => config.league_id,
+          'seasonId' => config.season_id
         }
 
         # If no period (aka, week) is specified, ESPN defaults to the
         # most recent (aka, current) matchup period
-        unless week.empty?
-          params["matchupPeriodId"] = week
-        end
+        params['matchupPeriodId'] = week unless week.empty?
 
-        param_string = params.map { |key, val| "#{key}=#{val}" }.join("&")
+        param_string = params.map { |key, val| "#{key}=#{val}" }.join('&')
         url = "http://games.espn.go.com/ffl/scoreboard?#{param_string}"
-        Lita.logger.debug("Searching for score at #{ url }")
+        Lita.logger.debug("Searching for score at #{url}")
 
         page = Nokogiri::HTML(open(url))
         matchups = page.xpath('//*[@class="ptsBased matchup"]')
 
-        resp["rows"] = matchups.map do |m|
-          rows = m.css("tr")
-          team_top  = rows[0].css("td.team div.name a").text
-          team_bottom = rows[1].css("td.team div.name a").text
-          score_top  = rows[0].css("td.score").text
-          score_bottom = rows[1].css("td.score").text
+        resp['rows'] = matchups.map do |m|
+          rows = m.css('tr')
+          team_top = rows[0].css('td.team div.name a').text
+          team_bottom = rows[1].css('td.team div.name a').text
+          score_top = rows[0].css('td.score').text
+          score_bottom = rows[1].css('td.score').text
 
           {
-            "team"  => "#{team_top}\n#{team_bottom}\n ",
-            "score" => "#{score_top}\n#{score_bottom}\n "
+            'team'  => "#{team_top}\n#{team_bottom}\n ",
+            'score' => "#{score_top}\n#{score_bottom}\n "
           }
         end
 
@@ -190,13 +188,13 @@ module Lita
       def espn_activity_scrape
         resp = []
         params = {
-          "leagueId" => config.league_id,
-          "seasonId" => config.season_id
+          'leagueId' => config.league_id,
+          'seasonId' => config.season_id
         }
 
-        param_string = params.map { |key, val| "#{key}=#{val}" }.join("&")
+        param_string = params.map { |key, val| "#{key}=#{val}" }.join('&')
         url = "http://games.espn.go.com/ffl/recentactivity?#{param_string}"
-        Lita.logger.debug("Searching for league activity at #{ url }")
+        Lita.logger.debug("Searching for league activity at #{url}")
         page = Nokogiri::HTML(open(url))
 
         # get activity and remove header rows
@@ -205,20 +203,20 @@ module Lita
         activity.each do |a|
           # TODO: check time stamps
           # TODO: timer instead of command
-          line = a.css("td")[2].inner_html
+          line = a.css('td')[2].inner_html
 
           # remove asterisks, as they'll conflict with markdown
-          line.gsub!(/\*/, "")
+          line.delete!('*')
 
           # convert bold formatting
-          line.gsub!(/<(\/)?b>/, "*")
+          line.gsub!(%r{<(/)?b>}, '*')
 
           # convert breaks to newlines
           line.gsub!(/<br>/, "\n")
 
           # add emoji
-          line.gsub!(/(\S+\sadded)/, ":green_heart: \\1")
-          line.gsub!(/(\S+\sdropped)/, ":broken_heart: \\1")
+          line.gsub!(/(\S+\sadded)/, ':green_heart: \\1')
+          line.gsub!(/(\S+\sdropped)/, ':broken_heart: \\1')
 
           resp << line
         end
@@ -227,9 +225,9 @@ module Lita
       end
 
       def format_results(raw)
-        rows = raw["rows"].map{|r| r.map{ |k,v| v}}
-        table = Terminal::Table.new(:headings => raw["headers"], :rows => rows)
-        "```\n#{ table }\n```"
+        rows = raw['rows'].map { |r| r.map { |_k, v| v } }
+        table = Terminal::Table.new(headings: raw['headers'], rows: rows)
+        "```\n#{table}\n```"
       end
 
       Lita.register_handler(self)
--- lita-espn-fantasy-football.gemspec
+++ lita-espn-fantasy-football.gemspec
@@ -1,30 +1,30 @@
 Gem::Specification.new do |s|
-  s.name          = "lita-espn-fantasy-football"
-  s.version       = "0.1.6"
-  s.authors       = ["Kevin Reedy", "Miles Evenson"]
-  s.email         = ["kevinreedy@gmail.com", "miles.evenson@gmail.com"]
-  s.description   = "Lita handler for ESPN Fantasy Football"
-  s.summary       = "Lita handler for ESPN Fantasy Football"
-  s.homepage      = "https://github.com/kevinreedy/lita-espn-fantasy-football"
-  s.license       = "Apache License, v2.0"
-  s.metadata      = { "lita_plugin_type" => "handler" }
+  s.name          = 'lita-espn-fantasy-football'
+  s.version       = '0.1.6'
+  s.authors       = ['Kevin Reedy', 'Miles Evenson']
+  s.email         = ['kevinreedy@gmail.com', 'miles.evenson@gmail.com']
+  s.description   = 'Lita handler for ESPN Fantasy Football'
+  s.summary       = 'Lita handler for ESPN Fantasy Football'
+  s.homepage      = 'https://github.com/kevinreedy/lita-espn-fantasy-football'
+  s.license       = 'Apache License, v2.0'
+  s.metadata      = { 'lita_plugin_type' => 'handler' }
 
-  s.files         = `git ls-files`.split($/)
+  s.files         = `git ls-files`.split($INPUT_RECORD_SEPARATOR)
   s.executables   = s.files.grep(%r{^bin/}) { |f| File.basename(f) }
   s.test_files    = s.files.grep(%r{^(test|spec|features)/})
-  s.require_paths = ["lib"]
+  s.require_paths = ['lib']
 
-  s.add_runtime_dependency "lita", ">= 4.6"
-  s.add_runtime_dependency "nokogiri", ">= 1.6"
-  s.add_runtime_dependency "terminal-table", ">= 1.5"
+  s.add_runtime_dependency 'lita', '>= 4.6'
+  s.add_runtime_dependency 'nokogiri', '>= 1.6'
+  s.add_runtime_dependency 'terminal-table', '>= 1.5'
 
-  s.add_development_dependency "bundler", "~> 1.3"
-  s.add_development_dependency "pry-byebug"
-  s.add_development_dependency "rake"
-  s.add_development_dependency "rack-test"
-  s.add_development_dependency "rspec", ">= 3.0.0"
-  s.add_development_dependency "simplecov"
-  s.add_development_dependency "coveralls"
-  s.add_development_dependency "lita-console"
-  s.add_development_dependency "rubocop", "~> 0.49"
+  s.add_development_dependency 'bundler', '~> 1.3'
+  s.add_development_dependency 'pry-byebug'
+  s.add_development_dependency 'rake'
+  s.add_development_dependency 'rack-test'
+  s.add_development_dependency 'rspec', '>= 3.0.0'
+  s.add_development_dependency 'simplecov'
+  s.add_development_dependency 'coveralls'
+  s.add_development_dependency 'lita-console'
+  s.add_development_dependency 'rubocop', '~> 0.49'
 end
